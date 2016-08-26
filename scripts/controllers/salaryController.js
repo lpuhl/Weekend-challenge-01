af61bee088f4993c0ea97373a30cd9f04d32da9d
Liz Puhl <eapuhl@gmail.com>
2016-08-26 10:14:42 -0500

    converted to angular, mostly

---
+++ scripts/controllers/salaryController.js
@@ -0,0 +1,32 @@
+var salaryApp = angular.module("salaryApp", []);
+
+salaryApp.controller("salaryController", ["$scope", function($scope) {
+  console.log("salary controller working!");
+  $scope.employee = {};
+  $scope.employees = [];
+  $scope.salaries = 0;
+
+  salaryTotal= function(employee) {
+    $scope.salaries += Math.round($scope.employee.salary/12*100)/100;
+  }
+
+  $scope.addEmployee = function() {
+    $scope.employee.first_name = $scope.first_name;
+    $scope.employee.last_name = $scope.last_name;
+    $scope.employee.id = $scope.employeeid;
+    $scope.employee.salary = $scope.employeesalary;
+    var newemp = angular.copy($scope.employee);
+    $scope.employees.push(newemp);
+    salaryTotal($scope.employee);
+    console.log($scope.employee);
+    console.log($scope.employees);
+    $scope.first_name = "";
+    $scope.last_name = "";
+    $scope.employeeid = "";
+    $scope.employeesalary = "";
+  }
+
+  // $scope.deleteEmployee = function() {
+  //   $scope.employee
+  // }
+}]);
